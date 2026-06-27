73d0ce269b22e9018ae32143363773d68fe06a17
swaraj404 <swarajpawar404@gmail.com>
2026-06-27 17:13:59 +0530

    learned about controllers and what they do

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query,Post, Body, Put } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -18,5 +20,21 @@ export class UserController {
             {id: 1, name: name || 'Swaraj Pawar'},
             {id: 2, name: 'Rudra Joshi'},
         ].filter(user => !name || user.name.toLowerCase().includes(name.toLowerCase()));
+    }
+  
+    //Param parameters
+    @Get(':id')
+    getUserById(@Param('id') id: string) {
+        return {id, name: 'Swaraj Pawar'};
+    }
+
+    @Post() //POST /user
+    createUser(@Body() CreateUserDto: CreateUserDto) {
+        return{ data: CreateUserDto, message: 'User created successfully'};
+    }
+
+    @Put(':id') //PUT /user/:id
+    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return {id, ...updateUserDto, message: 'User updated successfully'};
     }
 }
