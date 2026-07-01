231e22903902240d032c59a61e7175b631ff18f9
swaraj404 <swarajpawar404@gmail.com>
2026-07-01 11:12:19 +0530

    understanding the services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,38 @@
 import { Controller, Get, Param, Query,Post, Body, Put } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Get()
+    getUsers(@Query('name') name: string): unknown{
+        return this.userService.findAllUsers(name);
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: string): unknown{
+        return this.userService.findOneUserById(Number(id));
+    }
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto): unknown{
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Put(':id')
+    updateUser(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto): unknown{
+        return this.userService.updateUser(Number(id), UpdateUserDto);
+    }
+
+    @Put(':id')
+    deleteUser(@Param('id') id: string): unknown{
+        return this.userService.deleteUser(Number(id));
+    }
+    
+
     //GET /user
     // @Get()
     // getUser(){
@@ -14,27 +43,26 @@ export class UserController {
     // }
 
     //Query Parameters
-    @Get()
-    getUser(@Query('name') name: string) {
-        return [
-            {id: 1, name: name || 'Swaraj Pawar'},
-            {id: 2, name: 'Rudra Joshi'},
-        ].filter(user => !name || user.name.toLowerCase().includes(name.toLowerCase()));
-    }
+    // @Get()
+    // getUser(@Query('name') name: string) {
+    //     return [
+    //         {id: 1, name: name || 'Swaraj Pawar'},
+    //         {id: 2, name: 'Rudra Joshi'},
+    //     ].filter(user => !name || user.name.toLowerCase().includes(name.toLowerCase()));
+    // }
   
     //Param parameters
-    @Get(':id')
-    getUserById(@Param('id') id: string) {
-        return {id, name: 'Swaraj Pawar'};
-    }
+    // @Get(':id')
+    // getUserById(@Param('id') id: string) {
+    //     return {id, name: 'Swaraj Pawar'}; }
 
-    @Post() //POST /user
-    createUser(@Body() CreateUserDto: CreateUserDto) {
-        return{ data: CreateUserDto, message: 'User created successfully'};
-    }
+    // @Post() //POST /user
+    // createUser(@Body() CreateUserDto: CreateUserDto) {
+    //     return{ data: CreateUserDto, message: 'User created successfully'};
+    // }
 
-    @Put(':id') //PUT /user/:id
-    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return {id, ...updateUserDto, message: 'User updated successfully'};
-    }
+    // @Put(':id') //PUT /user/:id
+    // updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    //     return {id, ...updateUserDto, message: 'User updated successfully'};
+    // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LoggerService } from './user.logger';
+import { CreateUserDto } from './dto/create-user.dto';
 
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly logger: LoggerService) {}
+
+    private users: User[] = [
+        { id: 1, name: 'Swaraj Pawar', email: 'swaraj.pawar@example.com' },
+        { id: 2, name: 'Rudra Joshi', email: 'rudra.joshi@example.com' },
+    ];
+
+    findAllUsers(name: string= ' ') {
+        this.logger.log(`Fetching users with name filter: ${name}`);
+        return this.users.filter((user) => !name || user.name.toLowerCase().includes(name.toLowerCase()));
+    }
+
+    findOneUserById(id: number) {
+        const user = this.users.find((user) => user.id === id);
+        if (!user){
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+        return user;
+    }
+
+    createUser(dto: CreateUserDto) {
+        this.logger.log(`Creating user: ${dto.name}`);
+        const newUser: User = {
+            id: this.users.length + 1,
+            email: '', ...dto
+        };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    updateUser(id: number, updateUserDto: Partial<CreateUserDto>) {
+        this.logger.log(`Updating user with ID: ${id}`);
+        const userIndex = this.users.findIndex((user) => user.id === id);
+        if (userIndex === -1) {
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+        this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+        return this.users[userIndex];
+    }
+
+    deleteUser(id: number) {
+        this.logger.log(`Deleting user with ID: ${id}`);
+        const userIndex = this.users.findIndex((user) => user.id === id);
+        if (userIndex === -1) {
+            return null;
+        }
+        const deletedUser = this.users.splice(userIndex, 1);
+        return deletedUser[0];
+    }
+}
+    
+
+//Dependency chain looks like:
+//UserController -> needs UserService
+//UserService -> needs LoggerService
+//Nest -> creates and connects everything automatically.
